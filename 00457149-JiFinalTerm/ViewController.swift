843b4fd708e8a82ddcb9175c15952f47162b1bbe
Wilhelm Shen <33245687+WilhelmIllust@users.noreply.github.com>
2018-06-22 16:47:54 +0800

    Initial Commit

---
+++ 00457149-JiFinalTerm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  00457149-JiFinalTerm
+//
+//  Created by User11 on 2018/6/22.
+//  Copyright © 2018年 WilhelmShen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
